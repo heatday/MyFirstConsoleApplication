e38de52bb6277a2d407a533f6812356c432c8aa4
Daniel Muzo <luiscarloespi@gmail.com>
2023-04-22 15:03:39 -0500

    Add project files.

---
+++ MyFirstConsoleApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyFirstConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your name:");
+            string? name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your location:");
+            string? location = Console.ReadLine();
+
+            Console.WriteLine($"Hello my name is {name}.");
+            Console.WriteLine($"I am from {location}.");
+
+            DateTime currentDate = DateTime.Today;
+            Console.WriteLine($"The current date is {currentDate.ToString("MM/dd/yyyy")}.");
+
+            DateTime christmas = new DateTime(currentDate.Year, 12, 25);
+            int daysUntilChristmas = (christmas - currentDate).Days;
+            Console.WriteLine($"There are {daysUntilChristmas} days until Christmas.");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
